94e762c8bc4dfc5a8d154d3ff619b1686305592c
Akshay <akshaykhatri22@gmail.com>
2024-03-31 00:04:42 +0530

    server and mongoose setup with env

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("server is running...");
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+const app = express();
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 3000;
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.error("Error connecting to MongoDB", err));
