d06df90901c29cec870d9e694021d9cb42dff6fe
Akshay <akshaykhatri22@gmail.com>
2024-03-30 20:45:04 +0530

    Login Page Made and Register page sorted

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import user_icon from "../assets/images/person.png";
+import email_icon from "../assets/images/email.png";
+import password_icon from "../assets/images/password.png";
+import "../assets/css/RegisterLogin.css";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register">Register Page</Link>
+    <div className="container-register-login">
+      <div className="submit-container top-container">
+        <div className="submit">
+          <Link className="link gray" to="/register">
+            Register Page
+          </Link>
+        </div>
+        <div className="submit">Login</div>
+      </div>
+
+      <div className="inputs">
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="email" placeholder="Email" />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder="Password" />
+        </div>
+      </div>
+      <div className="forgot-password">
+        Lost Password? <span>Click Here!</span>
+      </div>
+      <div className="submit-container button-container">
+        <div className="submit button">Login</div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -6,22 +6,10 @@ import password_icon from "../assets/images/password.png";
 import "../assets/css/RegisterLogin.css";
 
 const Register = () => {
-  // return (
-  //   <div>
-  //     <h1>Register</h1>
-  //     <Link to="/login">Login Page</Link>
-  //   </div>
-  // );
-
   const [action, setAction] = useState("Register");
 
   return (
     <div className="container-register-login">
-      {/* <div className="header">
-        <div className="text">Register</div>
-        <div className="underline"></div>
-      </div> */}
-
       <div className="submit-container top-container">
         <div className="submit">Register</div>
         <div className="submit">
@@ -45,9 +33,7 @@ const Register = () => {
           <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forgot-password">
-        Lost Password? <span>Click Here!</span>
-      </div>
+
       <div className="submit-container button-container">
         <div className="submit button">Register</div>
       </div>
