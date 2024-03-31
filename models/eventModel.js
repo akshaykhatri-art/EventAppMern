4bf277c9afe59d3f1dbe9cb83951b4297667dcf9
Akshay <akshaykhatri22@gmail.com>
2024-03-31 21:53:15 +0530

    Event Model Schema

---
+++ models/eventModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    email: { type: String, required: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    organizerName: { type: String, required: true },
+    eventBanner: { type: String },
+    eventImages: [{ type: String }],
+    displayStatus: { type: Boolean, default: true },
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Event", eventSchema);
